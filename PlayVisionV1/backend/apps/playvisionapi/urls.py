82b3f5a559169d45b619f493afe0aad60caca8c0
alex <ac8369745@gmail.com>
2026-01-10 02:43:08 +0100

    Add Docker configuration, Django URLs, and Nginx setup for backend services

---
+++ PlayVisionV1/backend/apps/playvisionapi/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from rest_framework import routers
+from playvisionapi import views
+
+urlpatterns = [ 
+    path("api/home/", views.homepage),
+    #URLS for Teams
+    path("api/teams/", views.team_list),
+    path("api/teams/<str:title>/", views.team_details),
+    #URLS for Competitions
+    path("api/competitions/", views.competition_list),
+    path("api/competitions/<str:ctitle>/", views.competition_details),
+    path("api/competitions/<str:ctitle>/matches/", views.competition_matches),
+    #URLS for Player
+    path("api/players/<str:pname>/", views.player_details),
+    #URLS for Matchs
+    path("api/matchs/<int:matchid>/", views.match_details),
+    #URLS for Searchs
+    path("api/search/", views.search_page),
+    #URLS for Compare Players
+    path("api/compare/players/", views.compare_players),
+    #URLS for Most Searched Items
+    path("api/mostsearched/items", views.most_searched_items),
+    path("api/mostsearched/players", views.most_searched_players),
+]
