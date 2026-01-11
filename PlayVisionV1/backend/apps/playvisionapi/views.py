a199f69f9060f46a6d75f47a1c9af8b614868001
alex <ac8369745@gmail.com>
2026-01-11 18:43:33 +0100

    Add most searched items API endpoints for players, teams, and competitions

--- PlayVisionV1/backend/apps/playvisionapi/views.py
+++ PlayVisionV1/backend/apps/playvisionapi/views.py
@@ -346,6 +346,33 @@ def compare_players(request):
         "player2_data" : player2_serializer.data,
         "player2_season_stats" : player2_season_stat_serializer.data
     })
+
+@api_view (["GET"])
+def most_searched_items(request):
+    # In the future, implement logic to track and retrieve most searched items
+    most_searched_players_qs = Player.objects.all()[:5]
+    most_searched_teams_qs = Team.objects.all()[:5]
+    most_searched_competitions_qs = Competition.objects.all()[:5]
+
+    most_searched_competitions_serializer = CompetitionSerializer(most_searched_competitions_qs,many=True)
+    most_searched_teams_serializer = TeamSerializer(most_searched_teams_qs,many=True)
+    most_searched_players_serializer = PlayerSerializer(most_searched_players_qs,many=True)
+
+    return Response({
+        "most_searched_competitions": most_searched_competitions_serializer.data,
+        "most_searched_teams": most_searched_teams_serializer.data,
+        "most_searched_players": most_searched_players_serializer.data,
+    })
+
+@api_view (["GET"])
+def most_searched_players(request):
+    # In the future, implement logic to track and retrieve most searched players
+    most_searched_players_qs = Player.objects.all()[:10]
+    most_searched_players_serializer = PlayerSerializer(most_searched_players_qs,many=True)
+
+    return Response({
+        "most_searched_players": most_searched_players_serializer.data,
+    })
     
 match_stats_header = [
     {
